e3bff2b2f3d1180d5d4b7cb90ca3671444bc7d14
AminurRouf <aminur.rouf@ncl.ac.uk>
2016-10-20 16:06:14 +0100

    initial commit of code

---
+++ app/Models/OrcidModel.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp.Deserializers;
+
+namespace OrcidOauth2.Models
+{
+    public class Orcid
+    {
+        [DeserializeAs(Name = "orcid")]
+        public  string OrcidId { get; set; }
+
+        [DeserializeAs(Name = "Name")]
+        public string Name { get; set; }
+
+        [DeserializeAs(Name = "access_token")]
+        public string AccessToken { get; set; }
+
+        [DeserializeAs(Name = "token_type")]
+        public string TokenType { get; set; }
+
+        [DeserializeAs(Name = "refresh_token")]
+        public string RefreshToken { get; set; }
+
+        [DeserializeAs(Name = "expires_in")]
+        public string TokenExpiryDate { get; set; }
+  
+    }
+}
